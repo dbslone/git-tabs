b402c1ea28a3432f21018f24b068d2c5a50b21ad
David Slone <dbslone@gmail.com>
2017-03-30 14:21:28 -0400

    initial commit

---
+++ spec/git-tabs-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import GitTabs from '../lib/git-tabs';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('GitTabs', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('git-tabs');
+  });
+
+  describe('when the git-tabs:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.git-tabs')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'git-tabs:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.git-tabs')).toExist();
+
+        let gitTabsElement = workspaceElement.querySelector('.git-tabs');
+        expect(gitTabsElement).toExist();
+
+        let gitTabsPanel = atom.workspace.panelForItem(gitTabsElement);
+        expect(gitTabsPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'git-tabs:toggle');
+        expect(gitTabsPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.git-tabs')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'git-tabs:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let gitTabsElement = workspaceElement.querySelector('.git-tabs');
+        expect(gitTabsElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'git-tabs:toggle');
+        expect(gitTabsElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/git-tabs-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitTabsView from '../lib/git-tabs-view';
+
+describe('GitTabsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
