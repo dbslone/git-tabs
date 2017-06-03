47a6b727ccb48c5c039ac73c4bfef5600816bcb0
Valentin Agachi <github-com@agachi.name>
2017-06-03 11:35:08 +0200

    Confirm Notification feature

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "airbnb-base",
+  "globals": {
+    "atom": true
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    semi: [2, "never"]
+  }
+};
